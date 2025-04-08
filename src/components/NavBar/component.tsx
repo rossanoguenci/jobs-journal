e6f549f9221426a72811dac7dacd0dbe889f194f
Rox <rossanoguenci@users.noreply.github.com>
2025-04-08 14:48:51 +0100

    feat(ui): restyle user/app menu, add Settings & About pages, support light/dark theme

--- src/components/NavBar/component.tsx
+++ src/components/NavBar/component.tsx
@@ -5,12 +5,15 @@ import React from "react";
 import {
     Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, Button, Avatar
 } from "@heroui/react";
+import {Dropdown, DropdownItem, DropdownMenu, DropdownTrigger} from "@heroui/dropdown";
+import {useRouter} from "next/navigation";
 
 // import {useActionContext} from "@components/ActionProvider";
 // import {Avatar} from "@heroui/avatar";
 
 
 export default function Component() {
+    const router = useRouter();
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     // const {setActiveAction} = useActionContext();
@@ -23,32 +26,57 @@ export default function Component() {
     return (
         <Navbar isBordered onMenuOpenChange={setIsMenuOpen}>
             <NavbarContent>
-                {<NavbarMenuToggle
+
+                {/*<NavbarMenuToggle
                     aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                     className="sm:hidden"
-                />}
+                />*/}
+
                 <NavbarBrand>
                     <div className="flex items-center justify-between flex-col">
                         <p className="font-bold text-inherit">Jobs Journal</p>
-                        <p className="text-xs font-extralight text-inherit">- {process.env.NEXT_PUBLIC_VERSION} -</p>
-                        <p className="text-xs font-extralight text-inherit">({process.env.NEXT_PUBLIC_BUILD_NUMBER})</p>
                     </div>
                 </NavbarBrand>
             </NavbarContent>
 
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    {/*<Button size="sm" color="default" onPress={() => setActiveAction("add_application")}>Add application</Button>*/}
-                </NavbarItem>
-                <NavbarItem>
-                    <Avatar
-                        isBordered
-                        // as="button"
-                        name="U"
-                        size="sm"
-                    />
-                </NavbarItem>
+            {/*Dropdown menu*/}
+            <NavbarContent as="div" justify="end">
+                <Dropdown placement="bottom-end" backdrop="blur">
+                    <DropdownTrigger>
+                        <Avatar
+                            isBordered
+                            as="button"
+                            name="U"
+                            size="sm"
+                        />
+                    </DropdownTrigger>
+                    <DropdownMenu
+                        aria-label="Profile Actions"
+                        variant="flat"
+                    >
+
+                        <DropdownItem
+                            key="user" className="h-14 gap-2 cursor-default" isReadOnly>
+                            <p className="font-semibold">Hey there 👋</p>
+                        </DropdownItem>
+
+                        <DropdownItem key="profile_settings" startContent={<i className="bx bxs-briefcase"/>} href="/">Jobs list</DropdownItem>
+
+                        {/*<DropdownItem key="profile_settings">My Profile</DropdownItem>*/}
+
+                        <DropdownItem key="settings" startContent={<i className="bx bxs-cog"/>} href="/settings/">App
+                            Settings</DropdownItem>
+
+                        {/*<DropdownItem key="analytics">Analytics</DropdownItem>*/}
+
+                        {/*<DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>*/}
+
+                        <DropdownItem key="about" href="/about/" startContent={<i className="bx bxs-info-circle"/>}>About & Legal</DropdownItem>
+
+                    </DropdownMenu>
+                </Dropdown>
             </NavbarContent>
+
             {/*
             <NavbarMenu>
                 {menuItems.map((item, index) => (
@@ -64,6 +92,8 @@ export default function Component() {
                     </NavbarMenuItem>
                 ))}
             </NavbarMenu>*/}
+
+
         </Navbar>
     );
 }
